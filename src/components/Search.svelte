bd019f9de637031c6cd191a76cf7307aaa1bb852
Brenno Rodrigues <brennopereira6@gmail.com>
2023-01-22 15:40:30 -0300

    refactor: removing unused code

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,70 +1,77 @@
 <script lang="ts">
-    let searchValue: any = '';
-    
-    export let posts: any[];
+  let searchValue: any = "";
 
-    let postsFiltered: any[];
+  export let posts: any[];
 
-    const submitHandle = (e: any) => {
-      e.preventDefault()
-      location.replace(`${location.origin}/search?query=${searchValue}`)
-    }
+  let postsFiltered: any[];
 
-    $: {
-      postsFiltered = posts.filter(post => post.frontmatter.title.includes(searchValue))
-    }
+  $: {
+    postsFiltered = posts.filter((post) =>
+      post.frontmatter.title.includes(searchValue)
+    );
+
+    posts.forEach(post => {
+      if(post.frontmatter.title === searchValue)
+        location.pathname = post.url;
+    })
+  };
 </script>
 
 <div class="header-search">
-    <button class="header-icon-search">
-        <img
-            src="/search-icon.svg"
-            alt="Icone de pesquisa"
-        />
-    </button>
-    <form on:submit={submitHandle}>
-        <input
-            bind:value={searchValue}
-            list="posts-hints"
-            name="search"
-            type="search"
-            class="header-input-search"
-        />
-    </form>
+  <button class="header-icon-search">
+    <img src="/search-icon.svg" alt="Icone de pesquisa" />
+  </button>
+  <div>
+    <input
+      bind:value={searchValue}
+      list="posts-hints"
+      name="search"
+      type="search"
+      class="header-input-search"
+    />
+    {#if !postsFiltered.length}
+      <p class="header-error-search">Nenhum post encontrado.</p>
+    {/if}
+  </div>
 </div>
 
 <datalist id="posts-hints">
-  {#each postsFiltered as post}
-    <option value={post.frontmatter.title}/>
+  {#each posts as post}
+    <option value={post.frontmatter.title}>{post.frontmatter.title}</option>
   {/each}
 </datalist>
 
 <style>
-    .header-search {
-      display: flex;
-      margin-top: 1rem;
-      margin-left: auto;
-      margin-right: auto;
-      border-bottom: 2px solid var(--c-primary);
-      height: fit-content;
-    }
+  .header-search {
+    display: flex;
+    margin-top: 1rem;
+    margin-left: auto;
+    margin-right: auto;
+    height: fit-content;
+  }
 
-    .header-icon-search {
-      border: none;
-      background: none;
-      cursor: pointer;
-      width: 20px;
-      height: auto;
-    }
-  
-    .header-input-search {
-      width: 300px;
-      height: 30px;
-      background-color: transparent;
-      border: none;
-      color: var(--c-light-background);
-      font-size: 1.2rem;
-      padding-left: 20px;
-      padding: 5px;
-    }
+  .header-icon-search {
+    border: none;
+    background: none;
+    cursor: pointer;
+    width: 20px;
+    height: 31px;
+    border-bottom: 2px solid var(--c-primary);
+  }
+
+  .header-input-search {
+    background-color: transparent;
+    color: var(--c-light-background);
+    font-size: 1.2rem;
+    width: 300px;
+    height: 30px;
+    padding-left: 20px;
+    padding: 5px;
+    border: none;
+    border-bottom: 2px solid var(--c-primary);
+  }
+
+  .header-error-search {
+    color: var(--c-dark-focus);
+  }
 </style>
