be8aa4ec014016d02a185f31aa24adf0650057c7
João Castro <90338515+joaocstro@users.noreply.github.com>
2023-02-21 19:29:01 -0300

    feat: add splitted title to index schema

--- src/schemas/search.ts
+++ src/schemas/search.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const searchIndexSchema = z.object({
     title: z.string(),
+    titleWords: z.array(z.string()),
     publishedAt: z.string().datetime(),
     tags: z.array(z.string()),
     authorName: z.string(),
